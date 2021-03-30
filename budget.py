1bc7c2785f652149e2728f8f6d8624163d7c47b5
sachuu <sachintha3232@gmail.com>
2021-03-30 13:23:16 -0500

    push

--- budget.py
+++ budget.py
@@ -1,13 +1,29 @@
+import SpendObject
+
 income = 0
+keyword = ''
+totalBalance = 0
+monthlySpend = []
+lifetimeSpend = []
 
 while True:
+    #SETUP
     if 0 >= income:
-        income = int(input("What is your monthly income?"))
-        print("Ok Thank You")
+        income = int(input("What is your monthly income? "))
+        print("Got it, thank you! \n")
         expenses = int(input("Any expenses so far? You can put 0 as well"))
-        print("Got it")
+        print("Got  \n")
+        totalBalance = income - expenses
+    #PARSING FOR KEYWORDS
     else:
-        spent = int(input("Spent any money?"))
-        print("New total is")
-        income = income - spent
-        print(income)
+        print("Your balance is: ${} \n".format(totalBalance))
+        while(keyword != 'exit'):
+            keyword = input("Let me know if you need anything or type help to see all commands ")
+            if(keyword == 'add'):
+                newExpense = int(input("How much was spent?"))
+                expenseName = input("What did you spend it on?")
+                totalBalance = totalBalance - newExpense
+                print("Your new balance is ${}".format(totalBalance))
+            else:
+                print("Sorry I'm not sure what you mean?")
+        print("Thank you for using BudgetPal")
---
+++ classes.py
@@ -0,0 +1,4 @@
+class SpendObject:
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
